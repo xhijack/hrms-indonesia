2ec5667e39ce93040f8174156a3024eed09f07ef
Nanda <nandarestunailani@gmail.com>
2025-04-29 09:01:07 +0700

    Add salary register html

--- hrms_indonesia/hooks.py
+++ hrms_indonesia/hooks.py
@@ -26,7 +26,7 @@ app_license = "mit"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/hrms_indonesia/css/hrms_indonesia.css"
-# app_include_js = "/assets/hrms_indonesia/js/hrms_indonesia.js"
+app_include_js = "/assets/hrms_indonesia/js/salary_register.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/hrms_indonesia/css/hrms_indonesia.css"
